1fe80cc83c15c571d15cf536f075e97bc4a06c66
Mlie <emipa606@users.noreply.github.com>
2022-04-11 16:02:57 +0200

    Should now also ignore any age below teenager in the Age Matters mod

--- Source/Rainbeau's Rim Disorders/Main.cs
+++ Source/Rainbeau's Rim Disorders/Main.cs
@@ -7,15 +7,23 @@ public class Main
 {
     private static readonly bool ageMattersLoaded;
     private static readonly HediffDef babyHediffDef;
+    private static readonly HediffDef baby2HediffDef;
+    private static readonly HediffDef toddlerHediffDef;
+    private static readonly HediffDef childHediffDef;
 
     static Main()
     {
         ageMattersLoaded = ModLister.GetActiveModWithIdentifier("Troopersmith1.AgeMatters") != null;
-        if (ageMattersLoaded)
+        if (!ageMattersLoaded)
         {
-            Log.Message("[RimDisorders]: Adding compatibility patch for Age Matters");
-            babyHediffDef = HediffDef.Named("AgeMatters_baby");
+            return;
         }
+
+        Log.Message("[RimDisorders]: Adding compatibility patch for Age Matters");
+        babyHediffDef = HediffDef.Named("AgeMatters_baby");
+        baby2HediffDef = HediffDef.Named("AgeMatters_baby2");
+        toddlerHediffDef = HediffDef.Named("AgeMatters_toddler");
+        childHediffDef = HediffDef.Named("AgeMatters_child");
     }
 
     public static bool ShouldIgnoreDownedPawn(Pawn pawn)
@@ -25,6 +33,26 @@ public class Main
             return false;
         }
 
-        return pawn.health.hediffSet.HasHediff(babyHediffDef);
+        if (pawn.health.hediffSet.HasHediff(babyHediffDef))
+        {
+            return true;
+        }
+
+        if (pawn.health.hediffSet.HasHediff(baby2HediffDef))
+        {
+            return true;
+        }
+
+        if (pawn.health.hediffSet.HasHediff(toddlerHediffDef))
+        {
+            return true;
+        }
+
+        if (pawn.health.hediffSet.HasHediff(childHediffDef))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
