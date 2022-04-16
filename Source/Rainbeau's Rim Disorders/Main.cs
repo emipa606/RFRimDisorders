266855e5a5d462cec3c19c9f182193a52386c416
Mlie <emipa606@users.noreply.github.com>
2022-04-16 20:05:12 +0200

    Added support for [url=https://steamcommunity.com/sharedfiles/filedetails/?id=1541438230]Children, school and learning[/url]

--- Source/Rainbeau's Rim Disorders/Main.cs
+++ Source/Rainbeau's Rim Disorders/Main.cs
@@ -6,53 +6,68 @@ namespace RimDisorders;
 public class Main
 {
     private static readonly bool ageMattersLoaded;
+    private static readonly bool cslLoaded;
     private static readonly HediffDef babyHediffDef;
     private static readonly HediffDef baby2HediffDef;
     private static readonly HediffDef toddlerHediffDef;
     private static readonly HediffDef childHediffDef;
+    private static readonly HediffDef birthHediffDef;
 
     static Main()
     {
         ageMattersLoaded = ModLister.GetActiveModWithIdentifier("Troopersmith1.AgeMatters") != null;
-        if (!ageMattersLoaded)
+        if (ageMattersLoaded)
         {
-            return;
+            Log.Message("[RimDisorders]: Adding compatibility patch for Age Matters");
+            babyHediffDef = HediffDef.Named("AgeMatters_baby");
+            baby2HediffDef = HediffDef.Named("AgeMatters_baby2");
+            toddlerHediffDef = HediffDef.Named("AgeMatters_toddler");
+            childHediffDef = HediffDef.Named("AgeMatters_child");
         }
 
-        Log.Message("[RimDisorders]: Adding compatibility patch for Age Matters");
-        babyHediffDef = HediffDef.Named("AgeMatters_baby");
-        baby2HediffDef = HediffDef.Named("AgeMatters_baby2");
-        toddlerHediffDef = HediffDef.Named("AgeMatters_toddler");
-        childHediffDef = HediffDef.Named("AgeMatters_child");
+        cslLoaded = ModLister.GetActiveModWithIdentifier("Dylan.CSL") != null;
+        if (cslLoaded)
+        {
+            Log.Message("[RimDisorders]: Adding compatibility patch for Children School and Learning");
+            birthHediffDef = HediffDef.Named("ChildBirthInProgress");
+        }
     }
 
     public static bool ShouldIgnoreDownedPawn(Pawn pawn)
     {
-        if (!ageMattersLoaded)
+        if (!ageMattersLoaded && !cslLoaded)
         {
             return false;
         }
 
-        if (pawn.health.hediffSet.HasHediff(babyHediffDef))
+        if (ageMattersLoaded)
         {
-            return true;
+            if (pawn.health.hediffSet.HasHediff(babyHediffDef))
+            {
+                return true;
+            }
+
+            if (pawn.health.hediffSet.HasHediff(baby2HediffDef))
+            {
+                return true;
+            }
+
+            if (pawn.health.hediffSet.HasHediff(toddlerHediffDef))
+            {
+                return true;
+            }
+
+            if (pawn.health.hediffSet.HasHediff(childHediffDef))
+            {
+                return true;
+            }
         }
 
-        if (pawn.health.hediffSet.HasHediff(baby2HediffDef))
+        if (!cslLoaded)
         {
-            return true;
+            return false;
         }
 
-        if (pawn.health.hediffSet.HasHediff(toddlerHediffDef))
-        {
-            return true;
-        }
-
-        if (pawn.health.hediffSet.HasHediff(childHediffDef))
-        {
-            return true;
-        }
-
-        return false;
+        return pawn.health.hediffSet.HasHediff(birthHediffDef);
     }
 }
